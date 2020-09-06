c8ecb07369e9cb48f25785ca603fa3e7bd8b7ca6
sagar <avhad.avhad@gmail.com>
2020-09-06 20:52:05 +0530

    improved layout by removing hard coded resources like string, dimension, color

--- app/src/main/java/com/sagar/plobalapptask/ui/home/HomeActivity.kt
+++ app/src/main/java/com/sagar/plobalapptask/ui/home/HomeActivity.kt
@@ -11,20 +11,8 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
-        setSupportActionBar(toolbar)
-
         val navController = Navigation.findNavController(this, R.id.fragment)
         NavigationUI.setupWithNavController(nav_view, navController)
-        NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
     }
 
-    //back arrow toolbar
-
-    override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(
-            Navigation.findNavController(this, R.id.fragment),
-            drawer_layout
-        )
-    }
 }
